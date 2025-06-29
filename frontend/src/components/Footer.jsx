638cc3ceade1cd39560f4c2e3922247da7917928
Aniket kit <121393155+aniket-kit@users.noreply.github.com>
2025-06-29 17:20:17 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,41 +1,46 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
-  return (
-    <div>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+    return (
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-2/3 text-gray-600'>
-                hgjnkfghjkhgfcgjnjn
-            </p>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                <div>
+                    <img src={assets.logo} className='mb-5 w-32' alt="" />
+                    <p className='w-full md:w-2/3 text-gray-600'>
+                        hgjnkfghjkhgfcgjnjn
+                    </p>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <NavLink to='/'>
+                            <p>Home</p>
+                        </NavLink>
+                        <NavLink to='/about'>
+                            <p>About us</p>
+                        </NavLink>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>+91-2345676543</li>
+                        <li>aniketkit@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div >
+                <hr />
+                <p className='py-5 text-sm text-center'>
+                    © 2025 forever.com — All rights reserved.
+                </p>
+            </div>
         </div>
-        <div>
-            <p className='text-xl font-medium mb-5'>COMPANY</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
-        </div>
-        <div>
-            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+91-2345676543</li>
-                <li>aniketkit@gmail.com</li>
-            </ul>
-        </div>
-        </div>
-        <div >
-            <hr />
-            <p className='py-5 text-sm text-center'>
-                © 2025 forever.com — All rights reserved.
-            </p>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
